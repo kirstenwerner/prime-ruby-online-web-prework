9a61286bd35ea022d0ae8bad923d3f2284f885f7
Kirsten Werner <kirstenmwerner@gmail.com>
2019-01-31 20:17:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(value)
-  if value <= 1 || value == 0 || value == 1
+def prime?(int)
+  if int <= 1 || int == 0 || int == 1
     return false
   elsif
-    (2..value - 1).each do |i|
-      if value % i == 0
+    (2..int - 1).each do |i|
+      if int % i == 0
         return false
       end
     end
